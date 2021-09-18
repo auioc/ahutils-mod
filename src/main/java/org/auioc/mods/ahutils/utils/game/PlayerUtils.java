2b22df28b73700f1d6a7e9353ba4f54df28f6f45
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-18 19:49:02 +0800

    feat: get player's EntityRayTraceResult util

--- src/main/java/org/auioc/mods/ahutils/utils/game/PlayerUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/PlayerUtils.java
@@ -4,9 +4,17 @@ import javax.annotation.Nullable;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.EntityPredicates;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
 
 public interface PlayerUtils {
 
@@ -46,6 +54,30 @@ public interface PlayerUtils {
             player.position().toString(),
             (player.level == null) ? "~NULL~" : player.level.toString()
         );
+    }
+
+    static EntityRayTraceResult getEntityRayTraceResult(PlayerEntity player, double rayLength) {
+        World level = player.level;
+
+        Vector3d playerViewVector = player.getViewVector(0);
+        Vector3d rayPath = playerViewVector.scale(rayLength);
+
+        Vector3d from = player.getEyePosition(0);
+        Vector3d to = from.add(rayPath);
+
+        RayTraceContext rayCtx = new RayTraceContext(
+            from, to, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, player
+        );
+        BlockRayTraceResult rayHitBlock = level.clip(rayCtx);
+        if (rayHitBlock.getType() != RayTraceResult.Type.MISS) {
+            to = rayHitBlock.getLocation();
+        }
+
+        EntityRayTraceResult rayHitEntity = ProjectileHelper.getEntityHitResult(
+            level, player, from, to, player.getBoundingBox().expandTowards(to).inflate(1.0D), EntityPredicates.NO_SPECTATORS
+        );
+
+        return rayHitEntity;
     }
 
 }
