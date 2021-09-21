1d8391512b19763248014e9ecc5a53dcea63abc7
WakelessSloth56 <wsloth56@foxmail.com>
2021-09-22 00:46:43 +0800

    feat: rewrite method ProjectileHelper.getEntityHitResult() in EntityUtils

--- src/main/java/org/auioc/mods/ahutils/utils/game/EntityUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/EntityUtils.java
@@ -1,8 +1,12 @@
 package org.auioc.mods.ahutils.utils.game;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+import javax.annotation.Nullable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.util.EntityPredicates;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
@@ -10,6 +14,7 @@ import net.minecraft.util.math.RayTraceContext.BlockMode;
 import net.minecraft.util.math.RayTraceContext.FluidMode;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
 
 public interface EntityUtils {
 
@@ -20,6 +25,28 @@ public interface EntityUtils {
         Vector3d to = from.add(rayPath);
         return new Vector3d[] {from, to};
     }
+
+    @Nullable
+    public static EntityRayTraceResult getEntityHitResult(World world, Entity entity, Vector3d from, Vector3d to, AxisAlignedBB aabb, Predicate<Entity> predicate, float pickRadiusAddition) {
+        double d0 = Double.MAX_VALUE;
+        Entity targetEntity = null;
+
+        for (Entity entity1 : world.getEntities(targetEntity, aabb, predicate)) {
+            AxisAlignedBB entity1aabb = entity1.getBoundingBox().inflate((double) (entity1.getPickRadius() + pickRadiusAddition));
+            Optional<Vector3d> optional = entity1aabb.clip(from, to);
+            if (optional.isPresent()) {
+                double d1 = from.distanceToSqr(optional.get());
+                if (d1 < d0) {
+                    targetEntity = entity1;
+                    d0 = d1;
+                }
+            }
+        }
+
+        return targetEntity == null ? null : new EntityRayTraceResult(targetEntity);
+    }
+
+
 
     static BlockRayTraceResult getBlockRayTraceResult(Entity entity, double rayLength, BlockMode blockMode, FluidMode fluidMode) {
         Vector3d[] viewRay = getEntityViewRay(entity, rayLength);
