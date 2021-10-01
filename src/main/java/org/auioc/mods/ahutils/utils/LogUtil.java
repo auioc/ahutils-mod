dc0c0bec217a82f4c6d1718801c31d5abb116853
WakelessSloth56 <wsloth56@foxmail.com>
2021-10-01 20:17:45 +0800

    feat: LogUtil log with marker

--- src/main/java/org/auioc/mods/ahutils/utils/LogUtil.java
+++ src/main/java/org/auioc/mods/ahutils/utils/LogUtil.java
@@ -1,41 +1,12 @@
 package org.auioc.mods.ahutils.utils;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
 public class LogUtil {
-    public static void trace(String msg) {
-        getLogger().trace(msg);
-    }
-
-    public static void debug(String msg) {
-        getLogger().debug(msg);
-    }
-
-    public static void info(String msg) {
-        getLogger().info(msg);
-    }
-
-    public static void warn(String msg) {
-        getLogger().warn(msg);
-    }
-
-    public static void error(String msg) {
-        getLogger().error(msg);
-    }
-
-    public static void error(String msg, Throwable t) {
-        getLogger().error(msg, t);
-    }
-
-    public static void log(Level level, String message, Object... fields) {
-        getLogger().log(level, message, fields);
-    }
-
-    private static Logger getLogger() {
-        return LogManager.getLogger(getCaller().getClassName());
-    }
 
     private static StackTraceElement getCaller() {
         StackTraceElement[] callStack = Thread.currentThread().getStackTrace();
@@ -54,4 +25,78 @@ public class LogUtil {
         caller = callStack[i + 3];
         return caller;
     }
+
+    private static Logger getLoggerByCaller() {
+        return LogManager.getLogger(getCaller().getClassName());
+    }
+
+    private static Logger getModLogger() {
+        return LogManager.getLogger("ahutils");
+    }
+
+
+
+    public static void trace(String msg) {
+        getLoggerByCaller().trace(msg);
+    }
+
+    public static void debug(String msg) {
+        getLoggerByCaller().debug(msg);
+    }
+
+    public static void info(String msg) {
+        getLoggerByCaller().info(msg);
+    }
+
+    public static void warn(String msg) {
+        getLoggerByCaller().warn(msg);
+    }
+
+    public static void error(String msg) {
+        getLoggerByCaller().error(msg);
+    }
+
+    public static void error(String msg, Throwable t) {
+        getLoggerByCaller().error(msg, t);
+    }
+
+    public static void log(Level level, String message, Object... fields) {
+        getLoggerByCaller().log(level, message, fields);
+    }
+
+
+
+    public static Marker getMarker(String marker) {
+        return MarkerManager.getMarker(marker);
+    }
+
+
+    public static void trace(Marker marker, String msg) {
+        getModLogger().trace(marker, msg);
+    }
+
+    public static void debug(Marker marker, String msg) {
+        getModLogger().debug(marker, msg);
+    }
+
+    public static void info(Marker marker, String msg) {
+        getModLogger().info(marker, msg);
+    }
+
+    public static void warn(Marker marker, String msg) {
+        getModLogger().warn(marker, msg);
+    }
+
+    public static void error(Marker marker, String msg) {
+        getModLogger().error(marker, msg);
+    }
+
+    public static void error(Marker marker, String msg, Throwable t) {
+        getModLogger().error(marker, msg, t);
+    }
+
+    public static void log(Marker marker, Level level, String message, Object... fields) {
+        getModLogger().log(level, marker, message, fields);
+    }
+
 }
