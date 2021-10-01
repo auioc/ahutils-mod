0fbaa812b33b1efdad75e270bacaa66abb44d9c9
WakelessSloth56 <wsloth56@foxmail.com>
2021-10-01 22:02:51 +0800

    feat: LogUtil log with specified name

--- src/main/java/org/auioc/mods/ahutils/utils/LogUtil.java
+++ src/main/java/org/auioc/mods/ahutils/utils/LogUtil.java
@@ -1,6 +1,5 @@
 package org.auioc.mods.ahutils.utils;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
@@ -30,73 +29,113 @@ public class LogUtil {
         return LogManager.getLogger(getCaller().getClassName());
     }
 
-    private static Logger getModLogger() {
-        return LogManager.getLogger("ahutils");
+    public static Logger getNamedLogger(String name) {
+        return LogManager.getLogger(name);
     }
-
-
-
-    public static void trace(String msg) {
-        getLoggerByCaller().trace(msg);
-    }
-
-    public static void debug(String msg) {
-        getLoggerByCaller().debug(msg);
-    }
-
-    public static void info(String msg) {
-        getLoggerByCaller().info(msg);
-    }
-
-    public static void warn(String msg) {
-        getLoggerByCaller().warn(msg);
-    }
-
-    public static void error(String msg) {
-        getLoggerByCaller().error(msg);
-    }
-
-    public static void error(String msg, Throwable t) {
-        getLoggerByCaller().error(msg, t);
-    }
-
-    public static void log(Level level, String message, Object... fields) {
-        getLoggerByCaller().log(level, message, fields);
-    }
-
-
 
     public static Marker getMarker(String marker) {
         return MarkerManager.getMarker(marker);
     }
 
 
-    public static void trace(Marker marker, String msg) {
-        getModLogger().trace(marker, msg);
+
+    public static void trace(Object msg) {
+        getLoggerByCaller().trace(msg);
     }
 
-    public static void debug(Marker marker, String msg) {
-        getModLogger().debug(marker, msg);
+    public static void debug(Object msg) {
+        getLoggerByCaller().debug(msg);
     }
 
-    public static void info(Marker marker, String msg) {
-        getModLogger().info(marker, msg);
+    public static void info(Object msg) {
+        getLoggerByCaller().info(msg);
     }
 
-    public static void warn(Marker marker, String msg) {
-        getModLogger().warn(marker, msg);
+    public static void warn(Object msg) {
+        getLoggerByCaller().warn(msg);
     }
 
-    public static void error(Marker marker, String msg) {
-        getModLogger().error(marker, msg);
+    public static void error(Object msg) {
+        getLoggerByCaller().error(msg);
     }
 
-    public static void error(Marker marker, String msg, Throwable t) {
-        getModLogger().error(marker, msg, t);
+    public static void error(Object msg, Throwable t) {
+        getLoggerByCaller().error(msg, t);
     }
 
-    public static void log(Marker marker, Level level, String message, Object... fields) {
-        getModLogger().log(level, marker, message, fields);
+
+    public static void trace(Marker marker, Object msg) {
+        getNamedLogger("ahutils").trace(marker, msg);
+    }
+
+    public static void debug(Marker marker, Object msg) {
+        getNamedLogger("ahutils").debug(marker, msg);
+    }
+
+    public static void info(Marker marker, Object msg) {
+        getNamedLogger("ahutils").info(marker, msg);
+    }
+
+    public static void warn(Marker marker, Object msg) {
+        getNamedLogger("ahutils").warn(marker, msg);
+    }
+
+    public static void error(Marker marker, Object msg) {
+        getNamedLogger("ahutils").error(marker, msg);
+    }
+
+    public static void error(Marker marker, Object msg, Throwable t) {
+        getNamedLogger("ahutils").error(marker, msg, t);
+    }
+
+
+    public static void trace(String name, Object msg) {
+        getNamedLogger(name).trace(msg);
+    }
+
+    public static void debug(String name, Object msg) {
+        getNamedLogger(name).debug(msg);
+    }
+
+    public static void info(String name, Object msg) {
+        getNamedLogger(name).info(msg);
+    }
+
+    public static void warn(String name, Object msg) {
+        getNamedLogger(name).warn(msg);
+    }
+
+    public static void error(String name, Object msg) {
+        getNamedLogger(name).error(msg);
+    }
+
+    public static void error(String name, Object msg, Throwable t) {
+        getNamedLogger(name).error(msg, t);
+    }
+
+
+    public static void trace(String name, Marker marker, Object msg) {
+        getNamedLogger(name).trace(marker, msg);
+    }
+
+    public static void debug(String name, Marker marker, Object msg) {
+        getNamedLogger(name).debug(marker, msg);
+    }
+
+    public static void info(String name, Marker marker, Object msg) {
+        getNamedLogger(name).info(marker, msg);
+    }
+
+    public static void warn(String name, Marker marker, Object msg) {
+        getNamedLogger(name).warn(marker, msg);
+    }
+
+    public static void error(String name, Marker marker, Object msg) {
+        getNamedLogger(name).error(marker, msg);
+    }
+
+    public static void error(String name, Marker marker, Object msg, Throwable t) {
+        getNamedLogger(name).error(marker, msg, t);
     }
 
 }
