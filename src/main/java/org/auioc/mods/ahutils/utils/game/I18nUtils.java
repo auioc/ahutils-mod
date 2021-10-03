a19fd3970434a8224d595252bbacc5f370a29e30
WakelessSloth56 <wsloth56@foxmail.com>
2021-10-04 01:12:11 +0800

    refactor(utils): annotate I18nUtils as deprecated

--- src/main/java/org/auioc/mods/ahutils/utils/game/I18nUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/I18nUtils.java
@@ -3,6 +3,7 @@ package org.auioc.mods.ahutils.utils.game;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
+@Deprecated(since = "3.1.3")
 public interface I18nUtils {
     static ITextComponent getTranslatedText(String key) {
         return new TranslationTextComponent(key);
