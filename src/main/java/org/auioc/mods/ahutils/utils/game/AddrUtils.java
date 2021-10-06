e93a7a357445633ff246d9c0e16c841f6b6283bd
WakelessSloth56 <wsloth56@foxmail.com>
2021-10-06 20:52:46 +0800

    fix(utils): AddrUtils getIp method supports LocalAddress now

--- src/main/java/org/auioc/mods/ahutils/utils/game/AddrUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/AddrUtils.java
@@ -1,15 +1,26 @@
 package org.auioc.mods.ahutils.utils.game;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import javax.annotation.Nullable;
+import io.netty.channel.local.LocalAddress;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.NetworkManager;
 
 public interface AddrUtils {
 
+    @Nullable
     static String getIp(NetworkManager connection) {
-        return ((InetSocketAddress) connection.getRemoteAddress()).getAddress().getHostAddress();
+        SocketAddress addr = connection.getRemoteAddress();
+        if (addr instanceof InetSocketAddress) {
+            return ((InetSocketAddress) addr).getAddress().getHostAddress();
+        } else if (addr instanceof LocalAddress) {
+            return ((LocalAddress) addr).toString();
+        }
+        return null;
     }
 
+    @Nullable
     static String getPlayerIp(ServerPlayerEntity player) {
         return getIp(player.connection.getConnection());
     }
