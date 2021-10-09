f6332b22392ded7da73e7df1d96ce4d8194e972d
WakelessSloth56 <wsloth56@foxmail.com>
2021-10-09 16:32:38 +0800

    feat: triggerClientCrash command can be executed by the server only

--- src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandHandler.java
+++ src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandHandler.java
@@ -9,18 +9,34 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import org.auioc.mods.ahutils.common.command.argument.DamageSourceArgument;
 import org.auioc.mods.ahutils.common.network.PacketHandler;
 import org.auioc.mods.ahutils.utils.LogUtil;
+import org.auioc.mods.ahutils.utils.game.CommandUtils;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.ICommandSource;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.command.arguments.GameProfileArgument;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.DamageSource;
 
 public abstract class ServerCommandHandler {
 
     public static int triggerClientCrash(CommandContext<CommandSource> ctx, int mode) throws CommandSyntaxException {
-        Collection<GameProfile> targets = GameProfileArgument.getGameProfiles(ctx, "targets");
         CommandSource source = ctx.getSource();
+
+        ICommandSource privateSource;
+        try {
+            privateSource = CommandUtils.getPrivateSource(source);
+        } catch (Exception e) {
+            throw CommandUtils.REFLECTION_ERROR.create();
+        }
+
+        if (!(privateSource instanceof MinecraftServer)) {
+            throw CommandUtils.NOT_SERVER_ERROR.create();
+        }
+
+
+        Collection<GameProfile> targets = GameProfileArgument.getGameProfiles(ctx, "targets");
         String sourceName = source.getTextName() + ((source.getEntity() != null) ? "(" + source.getEntity().getStringUUID() + ")" : "");
 
         for (GameProfile gameprofile : targets) {
