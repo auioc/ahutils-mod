aa06a6eccbc5e3c667e74891468096267faa82f3
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-17 17:41:14 +0800

    refactor(porting): EnchUtils

--- src/main/java/org/auioc/mods/ahutils/utils/game/EnchUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/EnchUtils.java
@@ -1,10 +1,10 @@
 package org.auioc.mods.ahutils.utils.game;
 
 import java.util.Random;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public interface EnchUtils {
@@ -12,22 +12,22 @@ public interface EnchUtils {
         return ForgeRegistries.ENCHANTMENTS.getValue(new ResourceLocation(id));
     }
 
-    static void enchantAll(ListNBT enchantments) {
+    static void enchantAll(ListTag enchantments) {
         for (int i = 0; i < enchantments.size(); i++) {
-            CompoundNBT nbt = enchantments.getCompound(i);
+            CompoundTag nbt = enchantments.getCompound(i);
             nbt.putShort("lvl", (short) (nbt.getShort("lvl") + 1));
         }
     }
 
-    static void enchantOne(ListNBT enchantments, int index) {
-        CompoundNBT nbt = enchantments.getCompound(index);
+    static void enchantOne(ListTag enchantments, int index) {
+        CompoundTag nbt = enchantments.getCompound(index);
         nbt.putShort("lvl", (short) (nbt.getShort("lvl") + 1));
     }
 
-    static void enchantRandom(ListNBT enchantments) {
+    static void enchantRandom(ListTag enchantments) {
         int enchCount = enchantments.size();
         int index = (new Random()).nextInt((enchCount - 0) + 1) + 0;
-        CompoundNBT nbt = enchantments.getCompound(index);
+        CompoundTag nbt = enchantments.getCompound(index);
         nbt.putShort("lvl", (short) (nbt.getShort("lvl") + 1));
     }
 }
