1ef387c64cf7377446a208b2d185e9ab145bc168
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-17 17:34:55 +0800

    refactor(porting): CommandUtils

--- src/main/java/org/auioc/mods/ahutils/utils/game/CommandUtils.java
+++ src/main/java/org/auioc/mods/ahutils/utils/game/CommandUtils.java
@@ -2,23 +2,23 @@ package org.auioc.mods.ahutils.utils.game;
 
 import java.lang.reflect.Field;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import net.minecraft.command.CommandSource;
-import net.minecraft.command.ICommandSource;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.TranslatableComponent;
 
 public interface CommandUtils {
 
-    TranslationTextComponent INTERNAL_ERROR_MESSAGE = TextUtils.getI18nText("ahutils.command.failed.internal");
+    TranslatableComponent INTERNAL_ERROR_MESSAGE = TextUtils.getI18nText("ahutils.command.failed.internal");
 
     SimpleCommandExceptionType INTERNAL_ERROR = new SimpleCommandExceptionType(INTERNAL_ERROR_MESSAGE);
     SimpleCommandExceptionType NOT_SERVER_ERROR = new SimpleCommandExceptionType(TextUtils.getI18nText("ahutils.command.failed.not_server"));
     SimpleCommandExceptionType NOT_DEDICATED_SERVER_ERROR = new SimpleCommandExceptionType(TextUtils.getI18nText("ahutils.command.failed.not_dedicated_server"));
     SimpleCommandExceptionType REFLECTION_ERROR = new SimpleCommandExceptionType(TextUtils.getI18nText("ahutils.command.failed.reflection"));
 
-    static ICommandSource getPrivateSource(CommandSource source) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-        Field privateSourceField = CommandSource.class.getDeclaredField("source");
+    static CommandSource getPrivateSource(CommandSourceStack sourceStack) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field privateSourceField = CommandSourceStack.class.getDeclaredField("source");
         privateSourceField.setAccessible(true);
-        return (ICommandSource) privateSourceField.get(source);
+        return (CommandSource) privateSourceField.get(sourceStack);
     }
 
 }
