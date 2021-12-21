e818a2e5e4a227b3837b7d24b23bdb4046774e52
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-21 14:21:14 +0800

    feat: EntityDamageSourceArgument

--- src/main/java/org/auioc/mods/ahutils/common/command/CommandArgumentRegistry.java
+++ src/main/java/org/auioc/mods/ahutils/common/command/CommandArgumentRegistry.java
@@ -1,11 +1,13 @@
 package org.auioc.mods.ahutils.common.command;
 
 import org.auioc.mods.ahutils.common.command.argument.DamageSourceArgument;
+import org.auioc.mods.ahutils.common.command.argument.EntityDamageSourceArgument;
 import net.minecraft.commands.synchronization.ArgumentTypes;
 import net.minecraft.commands.synchronization.EmptyArgumentSerializer;
 
 public class CommandArgumentRegistry {
     public static void register() {
         ArgumentTypes.register("ahutils:damage_source", DamageSourceArgument.class, new EmptyArgumentSerializer<>(DamageSourceArgument::damageSource));
+        ArgumentTypes.register("ahutils:entity_damage_source", EntityDamageSourceArgument.class, new EmptyArgumentSerializer<>(EntityDamageSourceArgument::damageSource));
     }
 }
