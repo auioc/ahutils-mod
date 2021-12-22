2cd2be755b1ad7e5ffc60924c97126b754f04ee3
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-23 00:36:19 +0800

    feat: ip lookup command with additional info

--- src/main/java/org/auioc/mods/ahutils/server/command/impl/IpLookupCommand.java
+++ src/main/java/org/auioc/mods/ahutils/server/command/impl/IpLookupCommand.java
@@ -2,11 +2,16 @@ package org.auioc.mods.ahutils.server.command.impl;
 
 import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
+import static org.auioc.mods.ahutils.utils.game.TextUtils.getStringText;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import org.auioc.mods.ahutils.api.exception.HSimpleException;
 import org.auioc.mods.ahutils.utils.game.AddrUtils;
+import org.auioc.mods.ahutils.utils.network.AddressUtils;
+import org.auioc.mods.ahutils.utils.network.IpLookup;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.MutableComponent;
@@ -19,22 +24,63 @@ public class IpLookupCommand {
             .requires(source -> source.hasPermission(3))
             .then(
                 literal("player").then(
-                    argument("player", EntityArgument.player()).executes(ctx -> lookupPLayerIp(ctx, 1))
+                    argument("player", EntityArgument.player()).executes(ctx -> h(ctx, false, false, false))
+                        .then(
+                            literal("+asn").executes(ctx -> h(ctx, true, false, false))
+                                .then(
+                                    literal("+country").executes(ctx -> h(ctx, true, true, false))
+                                        .then(literal("+city").executes(ctx -> h(ctx, true, true, true)))
+                                )
+                                .then(literal("+city").executes(ctx -> h(ctx, true, false, true)))
+                        )
+                        .then(
+                            literal("+country").executes(ctx -> h(ctx, false, true, false))
+                                .then(literal("+city").executes(ctx -> h(ctx, false, true, true)))
+                        )
+                        .then(literal("+city").executes(ctx -> h(ctx, false, false, true)))
                 )
             );
 
-    private static int lookupPLayerIp(CommandContext<CommandSourceStack> ctx, int mode) throws CommandSyntaxException {
+    private static int h(CommandContext<CommandSourceStack> ctx, boolean asn, boolean country, boolean city) throws CommandSyntaxException {
         CommandSourceStack source = ctx.getSource();
 
         ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
         String ip = AddrUtils.getPlayerIp(player);
 
-        switch (mode) {
-            case 1: {
-                source.sendSuccess(((MutableComponent) player.getDisplayName()).append(": " + ip), false);
-                break;
+        MutableComponent message = getStringText("").append(player.getDisplayName()).append(": " + ip + " ");
+
+        if (AddressUtils.isLocalAddress(ip)) {
+            message.append("(Local)");
+        } else if (AddressUtils.isLanAddress(ip)) {
+            message.append("(LAN)");
+        } else {
+            if (!(asn | country | city)) {
+            } else {
+                message.append("(");
+                try {
+                    if (asn) {
+                        message.append("ASN: " + IpLookup.lookupAsn(ip, true) + (country | city ? ", " : ""));
+                    }
+                    if (country) {
+                        message.append("Country: " + IpLookup.lookupCountry(ip, true) + (city ? ", " : ""));
+                    }
+                    if (city) {
+                        message.append("City: ");
+                        try {
+                            message.append(IpLookup.lookupCity(ip, true));
+                        } catch (HSimpleException e) {
+                            message.append(getStringText("Unknown").withStyle(ChatFormatting.ITALIC));
+                        }
+                    }
+                } catch (Exception e) {
+                    source.sendFailure(getStringText("Lookup IP location failed: " + e.getMessage()));
+                    return Command.SINGLE_SUCCESS;
+                }
+                message.append(")");
             }
         }
+
+        source.sendSuccess(message, false);
 
         return Command.SINGLE_SUCCESS;
     }
