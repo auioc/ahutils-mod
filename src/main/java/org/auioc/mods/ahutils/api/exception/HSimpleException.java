db12891589ea62ff5f1b9f15ab68dca97a2ecd9e
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-22 23:35:07 +0800

    feat(api): HSimpleException

---
+++ src/main/java/org/auioc/mods/ahutils/api/exception/HSimpleException.java
@@ -0,0 +1,9 @@
+package org.auioc.mods.ahutils.api.exception;
+
+public class HSimpleException extends Exception {
+
+    public HSimpleException(String message) {
+        super(message);
+    }
+
+}
