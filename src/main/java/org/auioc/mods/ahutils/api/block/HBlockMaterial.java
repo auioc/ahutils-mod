5d5c0db1b0339530653d2ec6bf382e626f91a309
WakelessSloth56 <wsloth56@foxmail.com>
2021-12-28 11:46:01 +0800

    refactor(api): use HBlockMaterial directly

--- src/main/java/org/auioc/mods/ahutils/api/block/HBlockMaterial.java
+++ src/main/java/org/auioc/mods/ahutils/api/block/HBlockMaterial.java
@@ -5,65 +5,65 @@ import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.material.PushReaction;
 
 public class HBlockMaterial {
-    public static class Builder {
-        private PushReaction pushReaction = PushReaction.NORMAL;
-        private boolean blocksMotion = true;
-        private boolean flammable = false;
-        private boolean liquid = false;
-        private boolean replaceable = false;
-        private boolean solid = true;
-        private MaterialColor color = MaterialColor.NONE;
-        private boolean solidBlocking = true;
 
-        public Builder() {}
+    private PushReaction pushReaction = PushReaction.NORMAL;
+    private boolean blocksMotion = true;
+    private boolean flammable = false;
+    private boolean liquid = false;
+    private boolean replaceable = false;
+    private boolean solid = true;
+    private MaterialColor color = MaterialColor.NONE;
+    private boolean solidBlocking = true;
 
-        public HBlockMaterial.Builder color(MaterialColor color) {
-            this.color = color;
-            return this;
-        }
+    public HBlockMaterial() {}
 
-        public HBlockMaterial.Builder liquid() {
-            this.liquid = true;
-            return this;
-        }
-
-        public HBlockMaterial.Builder nonSolid() {
-            this.solid = false;
-            return this;
-        }
-
-        public HBlockMaterial.Builder noCollider() {
-            this.blocksMotion = false;
-            return this;
-        }
-
-        public HBlockMaterial.Builder notSolidBlocking() {
-            this.solidBlocking = false;
-            return this;
-        }
-
-        public HBlockMaterial.Builder flammable() {
-            this.flammable = true;
-            return this;
-        }
-
-        public HBlockMaterial.Builder replaceable() {
-            this.replaceable = true;
-            return this;
-        }
-
-        public HBlockMaterial.Builder destroyOnPush() {
-            this.pushReaction = PushReaction.DESTROY;
-            return this;
-        }
-
-        public HBlockMaterial.Builder notPushable() {
-            this.pushReaction = PushReaction.BLOCK;
-            return this;
-        }
-
-        public Material build() {
-            return new Material(this.color, this.liquid, this.solid, this.blocksMotion, this.solidBlocking, this.flammable, this.replaceable, this.pushReaction);
-        }
+    public HBlockMaterial color(MaterialColor color) {
+        this.color = color;
+        return this;
     }
+
+    public HBlockMaterial liquid() {
+        this.liquid = true;
+        return this;
+    }
+
+    public HBlockMaterial nonSolid() {
+        this.solid = false;
+        return this;
+    }
+
+    public HBlockMaterial noCollider() {
+        this.blocksMotion = false;
+        return this;
+    }
+
+    public HBlockMaterial notSolidBlocking() {
+        this.solidBlocking = false;
+        return this;
+    }
+
+    public HBlockMaterial flammable() {
+        this.flammable = true;
+        return this;
+    }
+
+    public HBlockMaterial replaceable() {
+        this.replaceable = true;
+        return this;
+    }
+
+    public HBlockMaterial destroyOnPush() {
+        this.pushReaction = PushReaction.DESTROY;
+        return this;
+    }
+
+    public HBlockMaterial notPushable() {
+        this.pushReaction = PushReaction.BLOCK;
+        return this;
+    }
+
+    public Material build() {
+        return new Material(this.color, this.liquid, this.solid, this.blocksMotion, this.solidBlocking, this.flammable, this.replaceable, this.pushReaction);
+    }
+
 }
