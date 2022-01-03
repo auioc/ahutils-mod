22e99c18d31cb665a6d826f24597fa4e44dba384
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-03 21:46:26 +0800

    refactor: remove hurt command

--- src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegistry.java
+++ src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegistry.java
@@ -3,7 +3,6 @@ package org.auioc.mods.ahutils.server.command;
 import static net.minecraft.commands.Commands.literal;
 import com.mojang.brigadier.CommandDispatcher;
 import org.auioc.mods.ahutils.AHUtils;
-import org.auioc.mods.ahutils.server.command.impl.HurtCommand;
 import org.auioc.mods.ahutils.server.command.impl.VersionCommand;
 import net.minecraft.commands.CommandSourceStack;
 
@@ -14,8 +13,6 @@ public class ServerCommandRegistry {
             literal(AHUtils.MOD_ID)
                 .executes(VersionCommand::getModVersion)
                 .then(literal("version").executes(VersionCommand::getModVersion))
-
-                .then(HurtCommand.BUILDER)
         );
     }
 
