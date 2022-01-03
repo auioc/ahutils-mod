125d778802b07418012d6e1d93232877f18f5080
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-03 22:13:00 +0800

    feat: new root command node

--- src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegistry.java
+++ src/main/java/org/auioc/mods/ahutils/server/command/ServerCommandRegistry.java
@@ -1,18 +1,26 @@
 package org.auioc.mods.ahutils.server.command;
 
 import static net.minecraft.commands.Commands.literal;
+import java.util.List;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.CommandNode;
 import org.auioc.mods.ahutils.AHUtils;
-import org.auioc.mods.ahutils.server.command.impl.VersionCommand;
 import net.minecraft.commands.CommandSourceStack;
 
 public class ServerCommandRegistry {
 
+    public static CommandNode<CommandSourceStack> getRootNode(CommandDispatcher<CommandSourceStack> dispatcher) {
+        CommandNode<CommandSourceStack> node = dispatcher.findNode(List.of("ah"));
+        if (node == null) {
+            node = dispatcher.register(literal("ah"));
+        }
+        return node;
+    }
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(
+        getRootNode(dispatcher).addChild(
             literal(AHUtils.MOD_ID)
-                .executes(VersionCommand::getModVersion)
-                .then(literal("version").executes(VersionCommand::getModVersion))
+                .build()
         );
     }
 
